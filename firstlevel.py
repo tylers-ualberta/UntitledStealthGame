0458707b0354fcd0c19970fdbefa439b25ec3af2
tylers-ualberta <tyms459@gmail.com>
2020-11-07 16:55:41 -0700

    Added resizable

--- firstlevel.py
+++ firstlevel.py
@@ -14,16 +14,21 @@ while running:
         if event.type == pygame.KEYDOWN:
             if event.key == K_ESCAPE:
                 running = False
+        
+        if event.type == pygame.VIDEORESIZE:
+            # There's some code to add back window content here.
+            surface = pygame.display.set_mode((event.w, event.h),
+                                              RESIZABLE|HWSURFACE|DOUBLEBUF)
 
     screen.fill((255, 255, 255))
 
-    pygame.draw.rect(screen, (0, 0, 0), ((150, 50), (50, 10)))  # ((posX, posY), (width (x), height (y)))
+    # ((posX, posY), (width (x), height (y)))
     # Position taken from top left corner, you provide the top left corner.
     pygame.draw.rect(screen, (0, 0, 0), ((0, 0), (screen.get_width(), 10)))
     pygame.draw.rect(screen, (0, 0, 0), ((0, 0), (10, screen.get_height())))
     pygame.draw.rect(screen, (0, 0, 0), ((0, screen.get_height()-10), (screen.get_width(), 10)))
     pygame.draw.rect(screen, (0, 0, 0), ((screen.get_width()-10, 0), (10, screen.get_height())))
-
+    player = pygame.draw.circle(screen, (0, 0, 255), (screen.get_width()-50, screen.get_height()-50), 10)
 
     pygame.display.flip()
 

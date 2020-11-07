88566e66e91a5d0754137300bf8e6eeaf2b37a8f
tylers-ualberta <tyms459@gmail.com>
2020-11-07 16:50:19 -0700

    Added resizable window, rectangles scale with window size

--- firstlevel.py
+++ firstlevel.py
@@ -1,7 +1,8 @@
 import pygame
+from pygame.locals import *
 pygame.init()
 
-screen = pygame.display.set_mode([1710, 900])
+screen = pygame.display.set_mode([500, 500], RESIZABLE|HWSURFACE|DOUBLEBUF)
 
 running = True
 
@@ -10,15 +11,18 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+        if event.type == pygame.KEYDOWN:
+            if event.key == K_ESCAPE:
+                running = False
 
     screen.fill((255, 255, 255))
 
     pygame.draw.rect(screen, (0, 0, 0), ((150, 50), (50, 10)))  # ((posX, posY), (width (x), height (y)))
     # Position taken from top left corner, you provide the top left corner.
-    pygame.draw.rect(screen, (0, 0, 0), ((0, 0), (500, 10)))
-    pygame.draw.rect(screen, (0, 0, 0), ((0, 0), (10, 500)))
-    pygame.draw.rect(screen, (0, 0, 0), ((0, 490), (500, 10)))
-    pygame.draw.rect(screen, (0, 0, 0), ((490, 0), (10, 500)))
+    pygame.draw.rect(screen, (0, 0, 0), ((0, 0), (screen.get_width(), 10)))
+    pygame.draw.rect(screen, (0, 0, 0), ((0, 0), (10, screen.get_height())))
+    pygame.draw.rect(screen, (0, 0, 0), ((0, screen.get_height()-10), (screen.get_width(), 10)))
+    pygame.draw.rect(screen, (0, 0, 0), ((screen.get_width()-10, 0), (10, screen.get_height())))
 
 
     pygame.display.flip()

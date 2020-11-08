e941846912ebb682d29c10dfcf042e92f7f1b624
unknown <smh1@ualberta.ca>
2020-11-08 08:43:04 -0700

    music

--- TitleScreen.py
+++ TitleScreen.py
@@ -25,7 +25,9 @@ StartRect.center = (630, 500)
 BackRect.center = (630,200)
 #Decor1 = pygame.image.load('Assets/PlayerWalking.png')
 
-
+pygame.mixer.init()
+pygame.mixer.music.load("widePutinSong.mp3")
+pygame.mixer.music.play()
 
 while Opening:
     

ec5a84ca808faeccdfac36459930b5f725e8cf77
tylers-ualberta <tyms459@gmail.com>
2020-11-08 00:16:23 -0700

    Modified main function, bug fixes, subclasses have started to be made

--- engine.py
+++ engine.py
@@ -10,8 +10,9 @@ class Player(pygame.sprite.Sprite):
             self.surf.fill((0, 0, 255))
             self.rect = self.surf.get_rect()
             self.rect.move_ip(offset)
+            print(self.rect)
         else:
-            self.rect = pygame.Rect(offset[0], offset[1], 20, 40)
+            self.rect = pygame.Rect(offset[0], offset[1], 15, 40)
             self.image = pygame.image.load(sprite)
 
     def update_position(self):
@@ -26,7 +27,7 @@ class Player(pygame.sprite.Sprite):
             self.rect.move_ip(2, 0)
         return
     
-    def draw(self):
+    def draw(self, screen):
         screen.blit(self.surf, self.rect)
         return
     
@@ -38,24 +39,44 @@ class Player(pygame.sprite.Sprite):
 class Enemy(pygame.sprite.Sprite):
     def __init__(self, offset, speed=0, colour=(255, 255, 0), sprite=""):
         super(Enemy, self).__init__()
-        if sprite == "":
-            self.surf = pygame.Surface((20,20))
-            self.surf.fill(colour)
-            self.rect = self.surf.get_rect()
-            self.rect.move_ip(offset)
-        else:
-            self.rect = pygame.Rect(offset[0], offset[1], 20, 40)
-            self.image = pygame.image.load(sprite)
-
+        self.surf = pygame.Surface((20,20))
+        self.surf.fill(colour)
+        self.rect = self.surf.get_rect()
+        self.offset = offset
         self.speed = speed
+        self.rect.move_ip(offset)
 
     def resize(self, dWidth, dHeight):
         self.rect.move_ip(-dWidth, -dHeight)
         return
 
-    def draw(self):
+    def draw(self, screen):
         screen.blit(self.surf, self.rect)
-        pass
+        return
+
+class Camera(Enemy):
+    def __init__(self, offset, sweep, colour=(255, 255, 0), sprite=""):
+        super().__init__(self, offset, 0, colour, sprite)
+        self.sweep = sweep
+        return
+
+
+class StationaryEnemy(Enemy):
+    def __init__(self, offset, sweep, colour=(255, 255, 0), sprite=""):
+        super().__init__(self, offset, 0, colour, sprite)
+        self.sweep = sweep
+        return
+
+
+class MovingEnemy(Enemy):
+    def __init__(self, offset, rang, speed=0, colour=(255, 255, 0), sprite=""):
+        super().__init__(self, offset, speed, colour, sprite)
+        self.range = rang
+        self.pos = offset
+        return
+
+    def update_position():
+        self.pos
 
 
 class Item(pygame.sprite.Sprite):
@@ -72,7 +93,7 @@ class Item(pygame.sprite.Sprite):
         self.rect.move_ip(-dWidth, -dHeight)
         return
     
-    def draw(self):
+    def draw(self, screen):
         screen.blit(self.surf, self.rect)
         pass
 
@@ -86,7 +107,7 @@ class Walls(pygame.sprite.Sprite):
         self.rect.move_ip(corner)
         return
     
-    def draw(self):
+    def draw(self, screen):
         screen.blit(self.surf, self.rect)
         return
 
@@ -127,12 +148,26 @@ def main():
     width, height = 1260, 700
     screen = pygame.display.set_mode([width, height], RESIZABLE|HWSURFACE|DOUBLEBUF)
     player = Player([1210, 650])
+    all_sprites = pygame.sprite.Group()
+    wall_sprites = pygame.sprite.Group()
+    enemies = pygame.sprite.Group()
+    items = pygame.sprite.Group()
+    all_sprites.add(player)
     # INCLUDE IN YOUR LEVEL:
     key = Item([400, 400])
     wallB1 = Walls([0, 0], screen.get_width(), 10)
     wallB2 = Walls([0, 0], 10, screen.get_height())
     wallB3 = Walls([0, screen.get_height()-10], screen.get_width(), 10)
     wallB4 = Walls([screen.get_width()-10, 0], 10, screen.get_height())
+    all_sprites.add(key)
+    all_sprites.add(wallB1)
+    all_sprites.add(wallB2)
+    all_sprites.add(wallB3)
+    all_sprites.add(wallB4)
+    wall_sprites.add(wallB1)
+    wall_sprites.add(wallB2)
+    wall_sprites.add(wallB3)
+    wall_sprites.add(wallB4)
     running = True
     clock = pygame.time.Clock()
     while running:
@@ -151,29 +186,34 @@ def main():
                 # Keeps the player's position constant
                 dw, dh = width - event.w, height - event.h
                 player.resize(dw, dh)
+                for wall in wall_sprites:
+                    wall.kill()
                 wallB1 = Walls([0, 0], screen.get_width(), 10)
                 wallB2 = Walls([0, 0], 10, screen.get_height())
                 wallB3 = Walls([0, screen.get_height()-10], screen.get_width(), 10)
                 wallB4 = Walls([screen.get_width()-10, 0], 10, screen.get_height())
+                wall_sprites.add(wallB1)
+                wall_sprites.add(wallB2)
+                wall_sprites.add(wallB3)
+                wall_sprites.add(wallB4)
+                all_sprites.add(wallB1)
+                all_sprites.add(wallB2)
+                all_sprites.add(wallB3)
+                all_sprites.add(wallB4)
                 width, height = event.w, event.h
                 
         # Sets screen colour
         screen.fill((255, 255, 255))
         # Draws boarder walls
-        wallB1.draw()
-        wallB2.draw()
-        wallB3.draw()
-        wallB4.draw()
-        key.draw()
-        # Updates player's position each loop
         player.update_position()
-        player.draw()
+        for entity in all_sprites:
+            screen.blit(entity.surf, entity.rect)
         pygame.display.flip()
         # Sets our framerate
         clock.tick(60)
     pygame.quit()
     return
+
 if __name__ == "__main__":
-    screen = pygame.display.set_mode([1260, 700], RESIZABLE|HWSURFACE|DOUBLEBUF)
     # main() used for testing new class functions
     main()

98f968468519ee58ab787b4ecd8cc28191428249
tylers-ualberta <tyms459@gmail.com>
2020-11-08 03:40:10 -0700

    New version of runningcode

--- runningcode.py
+++ runningcode.py
@@ -1,10 +1,27 @@
 # INCLUDE IN YOUR LEVEL:
-width, height = 1260, 700
-screen = pygame.display.set_mode([width, height], RESIZABLE|HWSURFACE|DOUBLEBUF)
+screen = pygame.display.set_mode([1260, 700], HWSURFACE|DOUBLEBUF)
 running = True
+# Add all sprites to their appropriate sprite group(s). Any sprites from the
+# assests folder need to be in all_sprites_img.
+all_sprites_surf = pygame.sprite.Group()
+all_sprites_img = pygame.sprite.Group()
+wall_sprites = pygame.sprite.Group()
+enemies = pygame.sprite.Group()
+items = pygame.sprite.Group()
 clock = pygame.time.Clock()
+wallB1 = Walls([0, 0], screen.get_width(), 10)
+wallB2 = Walls([0, 0], 10, screen.get_height())
+wallB3 = Walls([0, screen.get_height()-10], screen.get_width(), 10)
+wallB4 = Walls([screen.get_width()-10, 0], 10, screen.get_height())
+all_sprites_surf.add(wallB1)
+all_sprites_surf.add(wallB2)
+all_sprites_surf.add(wallB3)
+all_sprites_surf.add(wallB4)
+wall_sprites.add(wallB1)
+wall_sprites.add(wallB2)
+wall_sprites.add(wallB3)
+wall_sprites.add(wallB4)
 while running:
-
     # Handles quitting the game, resizing the window
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -12,25 +29,14 @@ while running:
         if event.type == pygame.KEYDOWN:
             if event.key == K_ESCAPE:
                 running = False
-        if event.type == pygame.VIDEORESIZE:
-            # There's some code to add back window content here.
-            surface = pygame.display.set_mode((event.w, event.h),
-                                            RESIZABLE|HWSURFACE|DOUBLEBUF)
-            # Keeps the player's position constant
-            dw, dh = width - event.w, height - event.h
-            player.resize(dw, dh)
-            width, height = event.w, event.h
             
     # Sets screen colour
     screen.fill((255, 255, 255))
-    # Draws boarder walls
-    pygame.draw.rect(screen, (0, 0, 0), ((0, 0), (screen.get_width(), 10)))
-    pygame.draw.rect(screen, (0, 0, 0), ((0, 0), (10, screen.get_height())))
-    pygame.draw.rect(screen, (0, 0, 0), ((0, screen.get_height()-10), (screen.get_width(), 10)))
-    pygame.draw.rect(screen, (0, 0, 0), ((screen.get_width()-10, 0), (10, screen.get_height())))
     # Updates player's position each loop
-    player.update_position()
-    player.draw()
+    for entity in all_sprites_surf:
+        screen.blit(entity.surf, entity.rect)
+    for entity in all_sprites_img:
+        screen.blit(entity.image, entity.rect)
     pygame.display.flip()
     # Sets our framerate
     clock.tick(60)

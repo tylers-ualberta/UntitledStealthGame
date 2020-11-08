860eba1370b1d9356e4887c1ec20a1d4f2893d49
Aaron L <aaronlin1100@gmail.com>
2020-11-08 05:06:55 -0700

    Added enemy 2

--- Level_1.py
+++ Level_1.py
@@ -18,44 +18,63 @@ def run():
     screen = pygame.display.set_mode([1260, 700], RESIZABLE|HWSURFACE|DOUBLEBUF)
     running = True
 
+    # Add all sprites to their appropriate sprite group(s). Any sprites from the
+    # assests folder need to be in all_sprites_img.
+    all_sprites_surf = pygame.sprite.Group()
+    all_sprites_img = pygame.sprite.Group()
+    wall_sprites = pygame.sprite.Group()
+    enemies = pygame.sprite.Group()
+    items = pygame.sprite.Group()
+    cone_sprites = pygame.sprite.Group()
+    clock = pygame.time.Clock()
+    wallB1 = Walls([0, 0], screen.get_width(), 10)
+    wallB2 = Walls([0, 0], 10, screen.get_height())
+    wallB3 = Walls([0, screen.get_height()-10], screen.get_width(), 10)
+    wallB4 = Walls([screen.get_width()-10, 0], 10, screen.get_height())
+    all_sprites_surf.add(wallB1)
+    all_sprites_surf.add(wallB2)
+    all_sprites_surf.add(wallB3)
+    all_sprites_surf.add(wallB4)
+    wall_sprites.add(wallB1)
+    wall_sprites.add(wallB2)
+    wall_sprites.add(wallB3)
+    wall_sprites.add(wallB4)
+
     # Entity spawn conditions
     # offset indicates player spawn point
-    offset = [30, screen.get_height()-180]
+    offset = [120, screen.get_height()-120]
     # Set enemy spawn
-    e_spawn1 = [3*screen.get_width()/4+20, screen.get_height()/2-10]
+    e_spawn1 = [300, 510]
+    e_spawn2 = [850, 650]
+    # Set goal location
+    goal = [50, screen.get_height()/5+20]
 
     # Initializing players and enemies
     player = Player(offset, sprite="Assets/Player.png")
-    enemy = Enemy(e_spawn1, sprite="Assets/SecurityGuard.png")
-    cone = Cone(e_spawn1, orientation="r")
+    enemy1 = Enemy(e_spawn1, sprite="Assets/SecurityGuard.png")
+    enemy2 = Enemy(e_spawn2, sprite="Assets/SecurityGuard.png")
+    cone1 = Cone(e_spawn1, orientation="d")
+    cone2 = Cone(e_spawn2, orientation="u")
+    endflag = Item(goal, sprite="Assets/EndFlag.png")
 
     start_ticks = pygame.time.get_ticks() #starter tick
 
     # Initialize Cone State
-    cone_state = 0
+    cone1_state = 0
+    cone2_state = 0
 
     # Start clock for clock.tick
     clock = pygame.time.Clock()
 
     while running:
+        # Handles quitting the game, resizing the window
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
-            if event.type == pygame.KEYDOWN:
-                if event.key == K_ESCAPE:
-                    running = False
-            if event.type == pygame.VIDEORESIZE:
-                # There's some code to add back window content here.
-                surface = pygame.display.set_mode((event.w, event.h),
-                                                  RESIZABLE|HWSURFACE|DOUBLEBUF)
-                player.resize()
+        if event.type == pygame.KEYDOWN:
+            if event.key == K_ESCAPE:
+                running = False
         screen.fill((255, 255, 255))
-
-        # Boundary rectangles
-        wallB1 = Walls([0, 0], screen.get_width(), 10)
-        wallB2 = Walls([0, 0], 10, screen.get_height())
-        wallB3 = Walls([0, screen.get_height()-10], screen.get_width(), 10)
-        wallB4 = Walls([screen.get_width()-10, 0], 10, screen.get_height())
 
         # Walls(corner(list), width, height, colour=(0,0,0))
         wall1 = Walls([0, screen.get_height()/3], 50, 10)
@@ -68,50 +87,83 @@ def run():
         wall8 = Walls([screen.get_width()/2, screen.get_height()/3], 10, 40)
         wall9 = Walls([screen.get_width()/2, screen.get_height()/2], 10, 120)
 
+        # Add entities to sprite group
+        all_sprites_img.add(player)
+        all_sprites_img.add(enemy1)
+        enemies.add(enemy1)
+        all_sprites_img.add(enemy2)
+        enemies.add(enemy2)
+        all_sprites_img.add(endflag)
+        items.add(endflag)
+        all_sprites_img.add(cone1)
+        cone_sprites.add(cone1)
+        all_sprites_img.add(cone2)
+        cone_sprites.add(cone2)
+
+        # Add walls to sprite group
+        all_sprites_surf.add(wall1)
+        all_sprites_surf.add(wall2)
+        all_sprites_surf.add(wall3)
+        all_sprites_surf.add(wall4)
+        all_sprites_surf.add(wall5)
+        all_sprites_surf.add(wall6)
+        all_sprites_surf.add(wall7)
+        all_sprites_surf.add(wall8)
+        all_sprites_surf.add(wall9)
+        wall_sprites.add(wall1)
+        wall_sprites.add(wall2)
+        wall_sprites.add(wall3)
+        wall_sprites.add(wall4)
+        wall_sprites.add(wall5)
+        wall_sprites.add(wall6)
+        wall_sprites.add(wall7)
+        wall_sprites.add(wall8)
+        wall_sprites.add(wall9)
+
         player.update_position()
 
         # Calculates time
         time = (pygame.time.get_ticks() - start_ticks) / 1000 #calculate how many seconds
         # Change view cone
-        if time > 1:
+        if time > 2:
             start_ticks = pygame.time.get_ticks()
         
-            if cone_state > 3:
-                cone_state = 0
-            if cone_state == 0:
-                cone.kill()
-                cone = Cone([3*screen.get_width()/4+20, screen.get_height()/2-10], orientation="d")
-            elif cone_state == 1:
-                cone.kill()
-                cone = Cone([3*screen.get_width()/4+20, screen.get_height()/2-10], orientation="r")
-            elif cone_state == 2:
-                cone.kill()
-                cone = Cone([3*screen.get_width()/4+20, screen.get_height()/2-10], orientation="u")
-            elif cone_state == 3:
-                cone.kill()
-                cone = Cone([3*screen.get_width()/4+20, screen.get_height()/2-10], orientation="r")
-            cone_state += 1
+        # Enemy 1
+            if cone1_state > 2:
+                cone1_state = 0
+            if cone1_state == 0:
+                cone1.kill()
+                cone1 = Cone([300, 510], orientation="r")
+                all_sprites_img.add(cone1)
+                cone_sprites.add(cone1)
+            elif cone1_state == 1:
+                cone1.kill()
+                cone1 = Cone([300, 510], orientation="d")
+                all_sprites_img.add(cone1)
+                cone_sprites.add(cone1)
+            cone1_state += 1
 
-        # Drawing
-        screen.blit(player.image, player.rect)
-        screen.blit(enemy.image, enemy.rect)
-        screen.blit(cone.image, cone.rect)
+        # Enemy 2
+            if cone2_state > 2:
+                cone2_state = 0
+            if cone2_state == 0:
+                cone2.kill()
+                cone2 = Cone([850, 650], orientation="r")
+                all_sprites_img.add(cone2)
+                cone_sprites.add(cone2)
+            elif cone2_state == 1:
+                cone2.kill()
+                cone2 = Cone([850, 650], orientation="u")
+                all_sprites_img.add(cone2)
+                cone_sprites.add(cone2)
+            cone2_state += 1
 
-        # Walls
-        screen.blit(wallB1.surf, wallB1.rect)
-        screen.blit(wallB2.surf, wallB2.rect)
-        screen.blit(wallB3.surf, wallB3.rect)
-        screen.blit(wallB4.surf, wallB4.rect)
-        screen.blit(wall1.surf, wall1.rect)
-        screen.blit(wall2.surf, wall2.rect)
-        screen.blit(wall3.surf, wall3.rect)
-        screen.blit(wall4.surf, wall4.rect)
-        screen.blit(wall5.surf, wall5.rect)
-        screen.blit(wall6.surf, wall6.rect)
-        screen.blit(wall7.surf, wall7.rect)
-        screen.blit(wall8.surf, wall8.rect)
-        screen.blit(wall9.surf, wall9.rect)
-        
+        # Print all sprites
+        for entity in all_sprites_surf:
+            screen.blit(entity.surf, entity.rect)
+        for entity in all_sprites_img:
+            screen.blit(entity.image, entity.rect)
+
         # Keep at bottom for display reasons
         pygame.display.flip()
         

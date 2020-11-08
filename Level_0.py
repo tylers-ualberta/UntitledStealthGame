2e2f710b530d468a334b723c1be1842b3b81048d
Aaron L <aaronlin1100@gmail.com>
2020-11-07 17:45:54 -0700

    Initialization of Level_0.py

---
+++ Level_0.py
@@ -0,0 +1,28 @@
+import pygame
+from pygame.locals import *
+pygame.init()
+""" Provide the necessary info for the main game to be able to generate the
+level.
+
+Level: 0
+
+Description: Tutorial level
+
+NOTES:
+Bottom right is 1260 x 700
+"""
+# offset indicates player spawn point
+offset = [50, 50]
+
+def walls(self):
+
+pass
+
+def enemy(self):
+"""
+
+"""
+
+pass
+
+pygame.quit()
